a0ad2280451d47e070f35924373bc23ad462110d
Jeremy Gibbons <jeremy.gibbons@laposte.net>
2018-01-26 10:22:05 -0600

    Simplify entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,6 @@
 #!/bin/sh
 set -e
 
-create_cache_dir() {
-  mkdir -p ${SQUID_CACHE_DIR}
-  chown -R ${SQUID_USER}:${SQUID_USER} ${SQUID_CACHE_DIR}
-}
-
-create_cache_dir
-
-
 # allow arguments to be passed to squid3
 if [[ ${1:0:1} = '-' ]]; then
   EXTRA_ARGS="$@"
@@ -20,10 +12,6 @@ fi
 
 # default behaviour is to launch squid
 if [[ -z ${1} ]]; then
-  if [[ ! -d ${SQUID_CACHE_DIR}/00 ]]; then
-    echo "Initializing cache..."
-    $(which squid) -N -f /etc/squid/squid.conf -z
-  fi
   echo "Starting squid..."
   exec $(which squid) -f /etc/squid/squid.conf -NYCd 1 ${EXTRA_ARGS}
 else
